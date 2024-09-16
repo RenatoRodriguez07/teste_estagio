2c47a9a8117eb7554bbb7818e53c0d35a5e1320a
Renato Rodrigues de Freitas <renatorofriguez07@gmail.com>
2024-09-16 01:02:29 -0300

    feat: criando sript para verificar números na sequencia de Fibonacci.

---
+++ src/fibonacci.py
@@ -0,0 +1,17 @@
+def fibonacci(limit):
+  fib_sequence = [0, 1]
+  while fib_sequence[-1] < limit:
+    fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+  return fib_sequence
+  
+def is_in_fibonacci(num):
+  fib_sequence = fibonacci(num)
+  if num in fib_sequence:
+    return f"O número {num} pertence à sequência de Fibonacci."
+  else:
+    return f"O número {num} NÃo pertence à sequência de Fibonacci."
+  
+numero = int(input("Informe um número: "))
+
+resultado = is_in_fibonacci(numero)
+print(resultado)
